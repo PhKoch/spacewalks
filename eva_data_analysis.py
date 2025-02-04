673ba4b1e3db2b9017ee27984d8b3152625a7363
Philipp Koch <philipp.koch@leibniz-fli.de>
2025-02-04 12:09:33 +0100

    Implement informative file names

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,9 +1,7 @@
 # https://data.nasa.gov/resource/eva.json (with modifications)
-#data_f = open('/home/sarah/Projects/ssi-ukrn-fair-course/data.json', 'r')
-#data_t = open('/home/sarah/Projects/ssi-ukrn-fair-course/data.csv','w')
-data_f = open('/home/philippk/Git/FAIRytail/astronaut-data-analysis-not-so-fair-main/data.json', 'r')
-data_t = open('/home/philippk/Git/FAIRytail/astronaut-data-analysis-not-so-fair-main/data.csv','w')
-g_file = 'myplot.png'
+data_f = open('./eva-data.json', 'r')
+data_t = open('./eva-data.csv','w')
+g_file = './cumulative_ev_graph.png'
 
 fieldnames = ("EVA #", "Country", "Crew    ", "Vehicle", "Date", "Duration", "Purpose")
 
